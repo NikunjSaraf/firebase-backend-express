0a71b272229e333d6a1efc537348605d1ffbf5ca
NikunjSaraf <nik.saraf1234@gmail.com>
2020-06-17 20:03:22 +0530

    first commit

---
+++ utils/config.js
@@ -0,0 +1,10 @@
+module.exports ={
+    apiKey: "AIzaSyDy32qL2UBIPMc4r8MLeo_WhqXvrxI9vLo",
+    authDomain: "react-social-app-2e14f.firebaseapp.com",
+    databaseURL: "https://react-social-app-2e14f.firebaseio.com",
+    projectId: "react-social-app-2e14f",
+    storageBucket: "react-social-app-2e14f.appspot.com",
+    messagingSenderId: "732533210246",
+    appId: "1:732533210246:web:fda4bf6762e967d315a507"
+
+}
